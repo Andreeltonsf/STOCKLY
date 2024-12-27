b0d47aa3a404a452cbbcca83b2be795dac28cf4e
Francisco ferreira <franciscoandreeltonsf@gmail.com>
2024-12-26 21:46:59 -0300

    feat: ensurece upsertProduct with next safe action

--- app/_actions/product/upsert-product/index.ts
+++ app/_actions/product/upsert-product/index.ts
@@ -1,51 +1,17 @@
 "use server";
 
 import { db } from "@/app/_lib/prisma";
-import { revalidatePath } from "next/cache";
-import { upsertProductSchema, UpsertProductSchema } from "./schema";
 import { actionClient } from "@/app/_lib/safe-action";
+import { revalidatePath } from "next/cache";
+import { upsertProductSchema } from "./schema";
 
 
 
-export const upsertProductAction = actionClient.schema(upsertProductSchema).action(async ({parsedInput}: Safe) => {})
-
-
-type SafeActionResult<T> = {
-  data?: T;
-  error?: string;
-};
-
-export async function upsertProduct(
-  data: UpsertProductSchema
-): Promise<SafeActionResult<{ success: boolean }> | undefined> 
-{
-  try {
-    upsertProductSchema.parse(data);
-
-    await db.product.upsert({ 
-      where: { id: data.id ?? "" },
-      update: {
-        name: data.name,
-        price: data.price,
-        stock: data.stock,
-      },
-      create: {
-        name: data.name,
-        price: data.price,  
-        stock: data.stock,
-      },
-    });
-
-    revalidatePath("/products");
-
-    return {
-      data: { success: true }
-    };
-
-  } catch (err) {
-    
-    return {
-      error: "Falha ao atualizar produto"
-    };
-  }
-}
+export const upsertProduct = actionClient.schema(upsertProductSchema).action(async ({ parsedInput : {id, ...data}}) => {
+  await db.product.upsert({
+    where: {id: id ?? ""},
+    update: data,
+    create: data,
+  });
+  revalidatePath("/products");
+})
