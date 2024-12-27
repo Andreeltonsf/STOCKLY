23bfa5ce5ac0176f6c8d92f980500231281b4dbc
Francisco ferreira <franciscoandreeltonsf@gmail.com>
2024-12-27 20:38:32 -0300

    feat: add productsDto

--- app/_data-acess/product/get-products.ts
+++ app/_data-acess/product/get-products.ts
@@ -1,12 +1,21 @@
-import { db } from "@/app/_lib/prisma"
-import { Product } from "@prisma/client"
+import { db } from "@/app/_lib/prisma";
+import { Product } from "@prisma/client";
 import { unstable_cache } from "next/cache";
-import "server-only"
+import "server-only";
 
 
-export const getProducts = async (): Promise<Product[]> => {
+export interface ProductDto extends Product{
+  status: "IN_STOCK" | "OUT_OF_STOCK";
+
+}
+
+
+export const getProducts = async (): Promise<ProductDto[]> => {
   const products = await db.product.findMany({})
-  return products
+  return products.map((product) => ({
+    ...product,
+    status: product.stock > 0 ? "IN_STOCK" : "OUT_OF_STOCK",
+  }));
 };
 
 
--- app/products/_components/table-columns.tsx
+++ app/products/_components/table-columns.tsx
@@ -1,10 +1,10 @@
 "use client"
 
 import { Badge } from "@/app/_components/ui/badge"
+import { ProductDto } from "@/app/_data-acess/product/get-products"
 import { ColumnDef } from "@tanstack/react-table"
 import { CircleIcon } from "lucide-react"
 import ProductDropdown from "./product-dropdawn"
-import { Product } from "@prisma/client"
 
 
 export type Payment = {
@@ -22,7 +22,7 @@ const getStatusLabel = (status:string) => {
 }
 
 
-export const ProductsTableColumns: ColumnDef<Product>[] = [
+export const ProductsTableColumns: ColumnDef<ProductDto>[] = [
   {
     accessorKey:"name",
     header: "Nome",
